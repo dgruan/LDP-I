df82d541e81c789d379ece39d01366c1b619065b
dgruan <douglasruan27@outlook.com>
2026-05-13 21:36:21 -0300

    Create Substituição em Vetor I.c

---
+++ C/Substituição em Vetor I.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int x[10], y;
+	
+	for(int i=0;i<10;i++){
+		scanf("%d",&y);
+		if(y==0 || y<0){
+			y=1;
+		}
+		x[i] = y;
+		printf("X[%d] = %d\n",i,x[i]);
+	}
+	return 0;
+}
